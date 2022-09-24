38e9c606827ee21466dcefc0ecf110dbf2d9a315
gmlwns2000 <gmlwns5176@gmail.com>
2022-09-25 05:23:38 +0900

    mp4

--- upscale/egvsr_test.py
+++ upscale/egvsr_test.py
@@ -156,34 +156,47 @@ if __name__ == '__main__':
     
     height, width = tuple([i * 4 for i in lr_shape])
 
-    fourcc = cv2.VideoWriter_fourcc(*'png ')
-    out = cv2.VideoWriter('saves/egvsr_test.mp4', fourcc, 24.0, (int(width), int(height)))
+    from PIL import Image
+    from subprocess import Popen, PIPE
+    from imutils.video import VideoStream
+    from imutils.object_detection import non_max_suppression
+    from imutils import paths
+    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    path = 'saves/egvsr_test.mp4'
+    # out = cv2.VideoWriter(', fourcc, 24.0, (int(width), int(height)))
 
-    frames_dir = './saves/frames/'
-    hr_prev = None
-    lr_prev = None
-    def batch_to_cv2mat(t, idx=0):
-        return torch.clamp(t[idx].detach().permute(1,2,0) * 255, 0, 255).to('cpu', non_blocking=True).numpy().astype(np.uint8)
-    for file_name in tqdm.tqdm(sorted(os.listdir(frames_dir))):
-        if file_name.endswith('.png'):
-            fname = os.path.join(frames_dir, file_name)
+    # ffmpeg setup
+    with Popen(['ffmpeg', '-y', '-f', 'image2pipe', '-vcodec', 'mjpeg', '-r', '24', '-i', '-',
+        '-vcodec', 'mpeg4', '-qscale', '5', '-r', '24', path], stdin=PIPE) as p:
 
-            img = cv2.cvtColor(cv2.imread(fname), cv2.COLOR_BGR2RGB)
-            img = torch.tensor(img, dtype=torch.float32).to(0, non_blocking=True).permute(2,0,1).unsqueeze(0) / 255.0
-            lr_curr = torch.nn.functional.interpolate(img, size=lr_shape, mode='area')
-            if lr_prev is None:
+        frames_dir = './saves/frames/'
+        hr_prev = None
+        lr_prev = None
+        def batch_to_cv2mat(t, idx=0):
+            return torch.clamp(t[idx].detach().permute(1,2,0) * 255, 0, 255).to('cpu', non_blocking=True).numpy().astype(np.uint8)
+        for file_name in tqdm.tqdm(sorted(os.listdir(frames_dir))):
+            if file_name.endswith('.png'):
+                fname = os.path.join(frames_dir, file_name)
+
+                img = cv2.cvtColor(cv2.imread(fname), cv2.COLOR_BGR2RGB)
+                img = torch.tensor(img, dtype=torch.float32).to(0, non_blocking=True).permute(2,0,1).unsqueeze(0) / 255.0
+                lr_curr = torch.nn.functional.interpolate(img, size=lr_shape, mode='area')
+                if lr_prev is None:
+                    lr_prev = lr_curr
+                if hr_prev is None:
+                    hr_prev = torch.nn.functional.interpolate(img, size=tuple([i * 4 for i in lr_shape]), mode='bicubic')
+                
+                with torch.no_grad():
+                    hr_curr = model(lr_curr, lr_prev, hr_prev)
+                
+                frame = batch_to_cv2mat(hr_curr)
+                #print(frame.shape, height, width)
+                #out.write(frame)
+                #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                im = Image.fromarray(frame)
+                im.save(p.stdin, 'JPEG')
+                hr_prev = hr_prev
                 lr_prev = lr_curr
-            if hr_prev is None:
-                hr_prev = torch.nn.functional.interpolate(img, size=tuple([i * 4 for i in lr_shape]), mode='bicubic')
-            
-            with torch.no_grad():
-                hr_curr = model(lr_curr, lr_prev, hr_prev)
-            
-            frame = cv2.cvtColor(batch_to_cv2mat(hr_curr), cv2.COLOR_RGB2BGR)
-            #print(frame.shape, height, width)
-            out.write(frame)
-            hr_prev = hr_prev
-            lr_prev = lr_curr
-    
-    out.release()
+        
+        #out.release()
             
